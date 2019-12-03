33b911de680c89b3bf2b7d51aa83c19553dcdb4f
Yvonne <tintinye@gmail.com>
2019-12-03 11:29:56 +0800

    123

--- didoPage/ViewController.swift
+++ didoPage/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate {
+   
+    
+    @IBOutlet var images: [UIImageView]!
+    
+    @IBOutlet weak var scroll: UIScrollView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet var topview: UIView!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = 4
+        pageControl.currentPage = 0
+        
+        scroll.delegate = self
+        
     }
 
+ func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+          let currentPage = Int(scroll.contentOffset.x / scroll.frame.size.width)
+        pageControl.currentPage = currentPage
+      }
 
+ func viewForZooming(in scroll: UIScrollView) -> UIView? {
+         for view in scroll.subviews {
+            if view.isKind(of: UIView.self) {
+              //   print ("ok")
+                return view
+               
+               }
+         }
+         return nil
+     }
+    
+ 
+   
+    
 }
 
